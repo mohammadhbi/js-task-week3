7f49df4642d2576ce9cb8a9d2e8d8b198c364bac
mohammad <mohammad19hobbi@gmail.com>
2025-02-04 23:30:11 +0330

    joke

--- mini JsProjects week3/flat/app.js
+++
@@ -1,8 +0,0 @@
-const input = [1, [2, [3, [4]], 5]] 
-function flatten(arr){
-arr.flat(Infinity);
-console.log(arr.flat(Infinity))
-}
-console.log(input);//before
-
-console.log(flatten(input)) //after
